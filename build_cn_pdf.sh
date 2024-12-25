2aead6091ff1bf8422e6c660fb5deb71eeb54bae
mark <mark_zjunlict@zju.edu.cn>
2024-12-25 14:20:21 +0800

    update to zj2025

--- build_cn_pdf.sh
+++ build_cn_pdf.sh
@@ -1,1 +1,1 @@
-asciidoctor-pdf -a pdf-theme=./resources/themes/basic-theme.yml -a pdf-fontsdir=./resources/fonts sslrules_cn.adoc
+asciidoctor-pdf -r asciidoctor-diagram -a pdf-theme=./resources/themes/basic-theme.yml -a pdf-fontsdir=./resources/fonts sslrules_cn.adoc
